b7000fd50e852737a8aac12f2f3f57daf8ba0cb1
Alex Meng <mengxp@inspur.com>
2023-12-21 09:31:35 +0800

    keymap: correct c-n/p in insert mode
    Signed-off-by: Alex Meng <mengxp@inspur.com>

--- lua/lazyvim/config/keymaps.lua
+++ lua/lazyvim/config/keymaps.lua
@@ -209,8 +209,8 @@ if vim.fn.has("nvim-0.11") == 0 then
 end
 
 -- mxp, expend editor command in insert mode
-map("i", "<C-n>", "<Up>")
-map("i", "<C-p>", "<Down>")
+map("i", "<C-p>", "<Up>")
+map("i", "<C-n>", "<Down>")
 map("i", "<C-b>", "<Left>")
 map("i", "<C-f>", "<Right>")
 map("i", "<C-a>", "<Home>")
