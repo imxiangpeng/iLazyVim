648bf24e4eb0dd6a8a7f0f3546f994f58b118282
Alex Meng <mengxp@inspur.com>
2025-02-08 16:05:27 +0800

    telescope prefer using fd not rg
    Signed-off-by: Alex Meng <mengxp@inspur.com>

--- lua/lazyvim/plugins/extras/editor/telescope.lua
+++ lua/lazyvim/plugins/extras/editor/telescope.lua
@@ -175,10 +175,10 @@ return {
       end
 
       local function find_command()
-        if 1 == vim.fn.executable("rg") then
-          return { "rg", "--files", "--color", "never", "-g", "!.git" }
-        elseif 1 == vim.fn.executable("fd") then
+        if 1 == vim.fn.executable("fd") then
           return { "fd", "--type", "f", "--color", "never", "-E", ".git" }
+        elseif 1 == vim.fn.executable("rg") then
+          return { "rg", "--files", "--color", "never", "-g", "!.git" }
         elseif 1 == vim.fn.executable("fdfind") then
           return { "fdfind", "--type", "f", "--color", "never", "-E", ".git" }
         elseif 1 == vim.fn.executable("find") and vim.fn.has("win32") == 0 then
